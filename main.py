9b6c5148108c8a9ecbccdac91e0f10ff9b6ab13c
Enrique Acosta Davila <enriqueacostad@gmail.com>
2025-04-07 16:02:55 -0600

    Commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class Cuadrado:
     def areacalc(self):
         return self.lado ** 2
     
-    
-
-        
+lado = float(input("Ingrese el valor del lado: "))
+exple = Cuadrado(lado)
+r = exple.area()
+print(f"El area es: {r}")
